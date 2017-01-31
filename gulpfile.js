48a99b15e1bc64aa9342683c9fb8acad96cfa12e
FrankLee <mc.franklee@gmail.com>
2017-02-01 00:49:47 +0800

    no message

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('resources/assets/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('resources/assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', [ 'sass:watch' ]);
