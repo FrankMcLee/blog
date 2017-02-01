c70fb9e32df5780270b6d01ca89a3a1d530cf7ce
FrankLee <mc.franklee@gmail.com>
2017-02-01 16:22:43 +0800

    chapter 5

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+/*Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/{id}', 'UsersController@show')->name('users.show');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{id}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{id}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{id}', 'UsersController2destroy')->name('users.destroy');*/
--- storage/framework/views/d7c4100ac04143a78e68c13b49ed48ad1addd7ed.php
+++ storage/framework/views/d7c4100ac04143a78e68c13b49ed48ad1addd7ed.php
@@ -9,6 +9,7 @@
 
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
+        <?php echo $__env->make('shared.messages', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
         <?php echo $__env->yieldContent('content'); ?>
         <?php echo $__env->make('layouts._footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
     </div>
